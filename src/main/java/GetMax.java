25c1e39c33346eca99406778462874b7e3a0e7e5
NEWYORKHEART911 <lavillelumiere8@gmail.com>
2023-02-03 05:38:13 +0000

    changes1

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxValue = Integer.MIN_VALUE;
+        for(int j=0; j<arr.length; j++) {
+            if(arr[j] > maxValue) {
+                maxValue = arr[j];
+            }
+        }
+        return maxValue;
     }
 }
